b8fa4c17be99e4f48619ff142c4896e711b54768
mihir-x <ictlancerx@gmail.com>
2023-09-15 01:37:43 +0600

    added the registration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 
 import './App.css'
 import AllCourse from './components/allcourses/AllCourse'
+import Registration from './components/registration/Registration'
 
 function App() {
 
   return (
     <>
-      <div className=' flex flex-col items-center max-w-screen-lg mx-auto p-2'>
+      <div className=' flex flex-col items-center max-w-screen-xl mx-auto p-2'>
         <h1 className=' text-4xl font-semibold my-4 md:my-10 text-center'>Course Registration</h1>
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+            <div className=' md:col-span-3'>
             <AllCourse></AllCourse>
+            </div>
+            <div>
+            <Registration></Registration>
+            </div>
         </div>
       </div>
     </>
---
+++ src/components/registration/Registration.jsx
@@ -0,0 +1,26 @@
+
+
+const Registration = () => {
+    return (
+        <div className="mt-4">
+            <div className="p-4 bg-white rounded-xl space-y-4">
+                <h1 className="text-xl text-[#2F80ED] font-bold bg-white">Credit Hour Remaining: </h1>
+                <hr />
+                <div>
+                    <h1 className=" bg-white text-xl font-bold text-[#1C1B1B]">Course Name</h1>
+                    <ol>
+                        {
+                            <li></li>
+                        }
+                    </ol>
+                </div>
+                <hr />
+                <h3 className=" text-lg text-[#1c1b1bcc] font-medium bg-white">Total Credit Hour :</h3>
+                <hr />
+                <h3 className=" text-lg text-[#1c1b1bcc] font-medium bg-white">Total Price :</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Registration;
