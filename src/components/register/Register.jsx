d3fa8510c4108df30df49546d73d87045e4c2188
mihir-x <ictlancerx@gmail.com>
2023-09-16 20:29:01 +0600

    fixed course title coloring

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Register = ({register}) => {
     const {course_title} = register
     return (
-        <li className='bg-white'>{course_title}</li>
+        <li className='bg-white text-[#1c1b1b99]'>{course_title}</li>
     );
 };
 
